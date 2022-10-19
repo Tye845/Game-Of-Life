b82477a1abfddeeb814131248cadec12f7ef994f
Tye Williams <114264890+Tye845@users.noreply.github.com>
2022-10-19 00:07:23 -0700

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,18 +1,8 @@
 import chn.util.*;
 import apcslib.*;
-/**
- * Write a description of class Tester here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class Tester
 {
-    // instance variables - replace the example below with your own
-
-    /**
-     * Constructor for objects of class Tester
-     */
     public static void main (String[] args)
     {
         FileInput inputTest = new FileInput("lifeNums.txt");
@@ -22,11 +12,10 @@ public class Tester
         test.countRow(10);
         test.countColumn(10);
         test.countTotal();
-        test.generate();//1
-        test.generate();//2
-        test.generate();//3
-        test.generate();//4
-        test.generate();
-        
+        test.generate(); //1
+        test.generate(); //2
+        test.generate(); //3
+        test.generate(); //4
+        test.generate();        
     }
 }
